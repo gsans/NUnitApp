1a7e0222b9df5c0dd5f899c67488cc80af0b00e8
Gerard <gerard.sans@gmail.com>
2014-02-24 00:43:54 +0000

    Initial with some tests.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            Total = 0;
+        }
+
+        public Calculator(int init)
+        {
+            Total = init;            
+        }
+
+        public int Total { get; set; }
+
+        public void Add(int a)
+        {
+            Total += a;
+        }
+
+        public void Reset()
+        {
+            Total = 0;
+        }
+    }
+}
---
+++ NUnitApp.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+using Calculator;
+
+namespace NUnitApp.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase(0, 1, Result = 1)]
+        [TestCase(1, 1, Result = 2)]
+        [TestCase(1, -1, Result = 0)]
+        public int Calculator_Add(int a, int b)
+        {
+            //Arrange
+            Calculator.Calculator c = new Calculator.Calculator(a);
+            //Act
+            c.Add(b);
+            //Assert
+            return c.Total;
+        }
+
+        [TestCase(0, 1, Result = 1, ExpectedException = typeof(ArgumentNullException))]
+        public int Calculator_Add_ArgumentNullException(int a, int b)
+        {
+            //Arrange
+            Calculator.Calculator c = new Calculator.Calculator(a);
+            //Act
+            c.Add(b);
+            throw new ArgumentNullException();
+            //Assert
+            return c.Total;
+        }
+    }
+}
